8621afdea79c665941d80f4e210d575903c30950
Max Eshleman <max@render.com>
2024-04-26 12:36:46 -0600

    web sevice

--- main.go
+++ main.go
@@ -26,10 +26,7 @@ func main() {
 		}
 	}()
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
-	fmt.Println("received exit signal")
+	defaultServer("8080")
 }
 
 func serveInterfaces() {
